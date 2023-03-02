c8e5b96a99e5a4a2561e929dc333cf2594a25109
MarcosAVZ <marcosavanzatti@gmail.com>
2023-03-02 20:16:31 -0300

    Segunda actividad

---
+++ ejercicio2.js
@@ -0,0 +1,15 @@
+
+const miPersona = {
+    nombre : "Marcos",
+    edad : 19,
+    "¿Eres desarrollador?" : true,
+    "Fecha_De_Nacimiento" : new Date(2003,3,16),
+    "Mi_Libro_Favorito" : {
+        Titulo : "El ingenioso hidalgo don Quijote de la Mancha",
+        Autor : "Miguel de Cervantes",
+        "Fecha" : new Date(1615,0,1),
+        url : "https://www.cervantesvirtual.com/obra-visor/el-ingenioso-hidalgo-don-quijote-de-la-mancha-6/html/05f86699-4b53-4d9b-8ab8-b40ab63fb0b3_2.html"
+    }
+}
+
+console.log(miPersona)
